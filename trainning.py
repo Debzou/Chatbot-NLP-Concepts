6f67d0f144e31f8d4f4e0644cfcce31b3a62cdc8
debzou <david.petit36@gmail.com>
2020-11-10 16:11:38 +0100

    commentary

--- trainning.py
+++ trainning.py
@@ -28,7 +28,7 @@ for intent in data_bot['intents']:
 remove_punctuation = ['?','.',',',';','!']
 # remove all punctuation 
 words = [stemming(w) for w in words if w not in remove_punctuation]
-# remove the duplicate words
+# remove the duplicate words (tips)
 words = sorted(set(words))
 tags = sorted(set(tags))
 
@@ -39,11 +39,15 @@ dim1_train = []
 dim2_train = []
 
 for (pattern,tag) in matrix:
+    # sentence transform into binary word 
     binList = matrice_of_word(pattern,words)
+    # add this sentence in dim1
     dim1_train.append(binList)
-
+    # find the tag corresponding to the sentence
     label = tags.index(tag)
+    # add this sentence in dim2
     dim2_train.append(label)
+    # print(binList,label)
 
 dim1_train = np.array(dim1_train)
 dim2_train = np.array(dim2_train)
@@ -51,4 +55,25 @@ dim2_train = np.array(dim2_train)
 print(dim1_train)
 print(dim2_train)
 
-#20min
+
+class DataOfChatbot(Dataset):
+    """
+    dataset is standarding by a model
+    https://stanford.edu/~shervine/blog/pytorch-how-to-generate-data-parallel
+    """
+    def __init__(self):
+        self.n = len(dim1_train)
+        self.dim1_train = dim1_train
+        self.dim2_train = dim2_train
+    # gather element 
+    def __getitem__(self, index):
+        return self.dim1_train[index],self.dim2_train[index]
+    # size 
+    def __len__(self):
+        return self.n    
+
+# hyperparameters
+batch_size = 8 # split the dataset in 8
+
+dataset = DataOfChatbot()
+train_loader = DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True,num_workers=2) # 2 threads
